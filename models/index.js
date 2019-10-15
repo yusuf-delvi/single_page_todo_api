57c034e8280892dccc523d3ab035451376cf69a8
yusuf-delvi <yusufmddelvi8@gmail.com>
2019-10-15 12:57:26 +0530

    change process.env.DB_URL in models/index

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const
   mongoose = require('mongoose'),
-  mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/todo_api';
+  mongoUrl = process.env.DB_URL || 'mongodb://localhost:27017/todo_api';
 
 mongoose.connect(mongoUrl, { useNewUrlParser: true });
 mongoose.Promise = Promise;
